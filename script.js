ea7cdf46a81164c0593483a2fa2727038157f5d4
Muhammad_Rehan <muhammad.rehan7140@gmail.com>
2022-10-16 23:37:02 -0400

    -m

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const mainContainer = document.getElementById("container");
 
+
 const baseUrl = "https://opentdb.com/api.php?amount=10"
+
+// const buttonAction = (event) => {
+//     const e = event.target;
+//     alert(`Button with ${e} was clicked `)
+
+// }
 
 fetch(baseUrl)
     .then(response => {
@@ -10,28 +17,50 @@ fetch(baseUrl)
         console.log(
             "Data", data
         )
-        for (let result of data.results) {
-            console.log("Question: ", result.question)
 
+        for (let result of data.results) {
+            /* Print out the question & correct answer in the console --------*/
+            console.log("Question: ", result.question)
+            console.log("Answer: ", result.correct_answer)
+
+            let incorrectAns = result.incorrect_answers.map(inc => {
+                console.log("INC => ", inc);
+            })
+            //--------------------------------------------------------------------------
+
+            // Display the 10 Questions on the WebPage ---------------------------------
             const questionsSection = document.createElement("ul");
-            const answerSection = document.createElement("button");
 
             const questionItem = document.createElement("li");
-            const answerItem = document.createElement("li");
 
             questionItem.innerHTML = result.question;
-            answerItem.innerHTML = result.correct_answer;
 
             questionsSection.append(questionItem)
-            answerSection.append(answerItem)
 
-            //Appends the Questions and Answers to the Webpage
-            mainContainer.append(questionsSection, answerSection)
-            //Creates a Border between every Question
+            mainContainer.append(questionsSection)
+            //--------------------------------------------------------------------------
+
+            //Appending all answer choices to a dictionary ------------------------------
+            x = result.correct_answer
+            var ansDict = {
+
+            }
+            ansDict[x] = "1";
+
+            let incAns = result.incorrect_answers.map(inc => {
+                ansDict[inc] = "0";
+            })
+            //-------------------------------------------------------------------------------
+            // Iterates through the dictionary and Displays all the answer choices----------
+            for (var ans in ansDict) {
+                const ansSection = document.createElement("button");
+                const ansItem = document.createElement("li");
+                ansItem.innerHTML = ans;
+                ansSection.append(ansItem);
+                mainContainer.append(ansSection);
+            }
+            //-----------------------------------------------------------------------------
+
             mainContainer.append(document.createElement("hr"))
-
-
-
-
         }
     })
